aafd4e4a2b2a4dfa82d53e8653d067de5e8bf28b
15019779076@163.com <aa11aa11>
2022-03-10 00:09:00 +0800

    update优化

--- src/main/java/com/yu/wrapper/core/lambda/LambdaUpdateBuild.java
+++ src/main/java/com/yu/wrapper/core/lambda/LambdaUpdateBuild.java
@@ -5,6 +5,18 @@ import com.yu.wrapper.core.toolkits.utils.lambdaUtils.BeanColumnFieldUtil;
 import com.yu.wrapper.core.toolkits.utils.lambdaUtils.SFunction;
 
 public interface LambdaUpdateBuild<ImplClass> extends UpdateBuild<ImplClass> {
+    default <T> ImplClass update(SFunction<T, ?> column, Object val) {
+        return update(true, column, val);
+    }
+
+    default <T> ImplClass update(boolean condition, SFunction<T, ?> column, Object val) {
+        return update(condition, column, val, null);
+    }
+
+    default <T> ImplClass update(SFunction<T, ?> column, Object val, String mapping) {
+        return update(true, column, val, mapping);
+    }
+
     /**
      * 查询字段（带泛型的vararg可能导致不安全，推荐使用BeanColumnFieldUtil.getColumnNamesByLambdas）
      */
